b3eb74a4d1221638e6425acba7c4f837bb3ffb2e
Ryanhustler420 <gouravgupta840@gmail.com>
2025-03-22 23:17:38 +0530

    ping-pong #2 added-ball

--- Day-3/game.py
+++ Day-3/game.py
@@ -10,6 +10,7 @@ BALL_SIZE = 10
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 PADDLE_SPEED = 5
+BALL_SPEED_X, BALL_SPEED_Y = 5, 5  # Ball movement speed
 
 # Set up display
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -18,6 +19,9 @@ pygame.display.set_caption("Ping Pong")
 # Paddles
 left_paddle = pygame.Rect(20, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
 right_paddle = pygame.Rect(WIDTH - 30, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+# Ball
+ball = pygame.Rect(WIDTH // 2 - BALL_SIZE // 2, HEIGHT // 2 - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
 
 # Game loop
 running = True
@@ -43,10 +47,19 @@ while running:
     if keys[pygame.K_DOWN] and right_paddle.bottom < HEIGHT:
         right_paddle.y += PADDLE_SPEED
 
+    # Move the ball
+    ball.x += BALL_SPEED_X
+    ball.y += BALL_SPEED_Y
+
+    # Ball collision with top and bottom walls
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        BALL_SPEED_Y = -BALL_SPEED_Y  # Reverse Y direction
+
     # Drawing
     screen.fill(BLACK)
     pygame.draw.rect(screen, WHITE, left_paddle)
     pygame.draw.rect(screen, WHITE, right_paddle)
+    pygame.draw.ellipse(screen, WHITE, ball)
 
     pygame.display.flip()  # Update display
 
