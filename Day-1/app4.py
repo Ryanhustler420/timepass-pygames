b03c1584c39d8c1a43082a58216d51abd053a6db
Gourav Gupta <gouravgupta840@gmail.com>
2025-03-23 20:14:21 +0530

    practice app4.py

---
+++ Day-1/app4.py
@@ -0,0 +1,46 @@
+import pygame
+import math
+import random as r
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+CENTER_X, CENTER_Y = WIDTH // 2, HEIGHT // 2
+RADIUS = 200
+
+points = []
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    
+    angle = math.atan2(mouse_y - CENTER_Y, mouse_x - CENTER_X)
+
+    x = CENTER_X + RADIUS * math.cos(angle)
+    y = CENTER_Y + RADIUS * math.sin(angle)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            points.append({ "angle": angle, "x": x, "y": y })
+            
+    for _p in points:
+        pygame.draw.circle(screen, (r.random() * 255, r.random() * 255, r.random() * 255), (int(_p['x']), int(_p['y'])), 5, 2)
+        t_x = CENTER_X + 5 * math.cos(math.radians(30))
+        t_y = CENTER_Y + 5 * math.sin(math.radians(30))
+        pygame.draw.line(screen, (0, 255, 0), (int(t_x), int(t_y)), (int(_p['x']), int(_p['y'])) , 5)
+    
+    pygame.draw.circle(screen, (r.random() * 255, r.random() * 255, r.random() * 255), (int(x), int(y)), 5, 2)
+
+    pygame.display.flip()
+    clock.tick(300) # 60 FPS
+            
+pygame.quit()
